532517e4555ff9314cbad4e2a1dc75b7e39401e5
webochen <cwb13220430087@163.com>
2025-03-26 23:02:38 +0800

    继续消化面向对象编程例子

--- 修改属性2.py
+++ 修改属性2.py
@@ -1,18 +1,24 @@
 class Employee:
-    def __init__(self,name,salary,value):
-        Employee.printclass(self,name,salary,'age',value)
+    def __init__(self,name,salary,age_value):
+
         self.name = name
         self.salary = salary
-    @classmethod
-    def printclass(cls,obj,name,salary,age,value):
-        if not hasattr(obj, age,):
-            setattr(obj,age,value)
-        else:
-            print(f"{name}'s salary is {salary},his age is {age}")
+        has_age = hasattr(self,'age')#hasattr判断有无参数，注意该参数要用引号
+        print(has_age)#直接打印判断结果
+        if not has_age:#not False即为True，运行下面添加步骤
+
+            setattr(self,'age',age_value)#添加该参数并将传入的参数作为age的值，注意参数要用引号
+        self.printclass()#调用打印方法
+
+    def printclass(self):#打印方法
+
+            print(f"{self.name}'s salary is {self.salary},his age is {self.age}")
 
 
 name=input()
 salary=int(input())
 age=int(input())
-e=Employee(name,salary,age)
-e.printclass()
+e=Employee(name,salary,age)#创建实例e
+
+
+
--- 教务系统实例.py
+++ 教务系统实例.py
@@ -1,4 +1,13 @@
-class Student:#学生类
+"""本例通过两个父类分别定义学生系统和课程系统，学生系统包括学号，姓名，以及学生选课记录变量，课程类包括课程ID，
+课程名称，学分以及选课容量属性和初始化为0的当前选课人数，在通过Enrollment类将课程系统和学生系统关联，并且在此类中
+通过@classmethon定义类方法用来判断课程容量是否还可选，在__init__下面先进行判断是否可选，然后在操作实例属性
+此类中还有类变量all_enrollments用来储存全局选课记录，类变量通常用来记录全局变量，本例中即为记录全局选课记录，通过append方法
+将选课情况添加进对应变量中，"""
+
+
+
+
+class Student:#学生类，无类变量
     def __init__(self, student_id: str, name: str):#两个参数，学号，姓名
         self.student_id = student_id#实例属性
         self.name = name#实例属性
@@ -7,20 +16,20 @@ class Student:#学生类
 
 class Course:#课程类，定义课程的基本属性
     def __init__(self, course_id: str, name: str, credit: int, capacity: int):
-        self.course_id = course_id#课程编号
-        self.name = name#课程名称
-        self.credit = credit#学分
-        self.capacity = capacity#最大容量通过最大容量和当前选课人数实现选课人数控制
+        self.course_id = course_id#课程编号，实例属性
+        self.name = name#课程名称，实例属性
+        self.credit = credit#学分，实例属性
+        self.capacity = capacity#最大容量通过最大容量和当前选课人数实现选课人数控制，实例属性
         self.current_students = 0#当前选课人数
 
 
 class Enrollment:
-    all_enrollments = []#记录全局选课记录
+    all_enrollments = []#记录全局选课记录，类变量，储存类级别的公共数据，例如统计实例数量，本类变量为储存全局选课记录这个统计过的实例数量
 
     def __init__(self, student: Student, course: Course):#关联属性
         if self.check_availability(course):#调用类方法检查课程容量
-            self.student = student#关联学生
-            self.course = course#关联课程类
+            self.student = student#关联学生,实例属性
+            self.course = course#关联课程类，实例属性
             self.grade = None#初始化成绩
             student.enrolled_courses.append(self)#更新选课列表
             course.current_students += 1#更新选课人数
